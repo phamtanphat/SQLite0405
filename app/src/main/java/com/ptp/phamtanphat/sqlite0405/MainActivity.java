807367e4cb46af0c7244861a1da072c4d7010f34
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-25 18:34:26 +0700

    "Create Table"

--- app/src/main/java/com/ptp/phamtanphat/sqlite0405/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/sqlite0405/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    SQLite sqLite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Tao ra database
+        sqLite = new SQLite(MainActivity.this,"Quanlymonan.sql",null,1);
+        // Tao ra bang
+
+        String createTable = "CREATE TABLE IF NOT EXISTS Nhahang(Id INTEGER PRIMARY KEY AUTOINCREMENT , Tenmonan VARCHAR , Giamonan INTEGER , Diadiem VARCHAR )";
+        sqLite.QueryData(createTable);
     }
 }
+
+
